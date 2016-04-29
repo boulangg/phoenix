fbe46ac140804cd62ebefc770052dba09ea87939
boulangg <guillaume.boulanger68@sfr.fr>
2016-04-29 16:48:46 +0300

    Fix missing license header

--- kernel/lib/libc/src/errno.c
+++ kernel/lib/libc/src/errno.c
@@ -4,20 +4,6 @@
  * The license is available in the LICENSE file or at https://github.com/boulangg/phoenix/blob/master/LICENSE
  */
 
-#ifndef _ERRNO_H_
-#define _ERRNO_H_
+#include <errno.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-extern int errno;
-
-#ifdef __cplusplus
-}
-#endif
-
-#define EINVAL	1
-#define ENOMEM	2
-
-#endif // _ERRNO_H_
+int errno=0;
--- kernel/lib/libc/src/memfn.c
+++ kernel/lib/libc/src/memfn.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2016 Boulanger Guillaume, Chathura Namalgamuwa
+ * The file is distributed under the MIT license
+ * The license is available in the LICENSE file or at https://github.com/boulangg/phoenix/blob/master/LICENSE
+ */
+
 #include <string.h>
 
 void* memcpy(void* restrict dst, const void* restrict src, size_t count)
