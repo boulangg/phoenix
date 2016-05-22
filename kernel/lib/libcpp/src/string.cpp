f8c0e048ef73032ddc782a9c3a1cf36f0790112a
boulangg <guillaume.boulanger68@sfr.fr>
2016-05-22 13:56:43 +0300

    Improve string and utility

--- kernel/lib/libcpp/src/string.cpp
+++ kernel/lib/libcpp/src/string.cpp
@@ -7,52 +7,47 @@
 #include <string>
 #include <utility>
 
+#define DEFAULT_CAPACITY 4
 
-size_t std::string::length(const char *s) {
+namespace std {
+
+size_t string::length(const char *s) {
     size_t len = 0;
     while(*s++!='\0')
         len++;
     return len;
 }
 
-std::string::string() {
+string::string(): _size(0), _capacity(DEFAULT_CAPACITY), _data(new char[_capacity]) {
 
 }
 
-std::string::string(const char *s): _size(length(s)),_capacity(std::nearest_power_2(_size+1)),_data(new char[_capacity]) {
+string::string(const char *s): _size(length(s)),_capacity(nearest_power_2(_size+1)),_data(new char[_capacity]) {
     for(size_t i=0;i<_size;i++)
         _data[i]=s[i];
     _data[_size]='\0';
 }
 
-std::string::string(const string& s): _size(s._size),_capacity(s._capacity),_data(new char[_capacity]) {
+string::string(const string& s): _size(s._size),_capacity(s._capacity),_data(new char[_capacity]) {
     for(size_t i=0;i<_size+1;i++)
         _data[i] = s._data[i];
 }
 
-std::string::string(string&& s): _size(s._size), _capacity(s._capacity),_data(s._data) {
+string::string(string&& s): _size(s._size), _capacity(s._capacity),_data(s._data) {
     s._data = nullptr;
     s._size =0;
     s._capacity=0;
 }
 
-std::string& std::string::operator+=(const string& str) {
-    if(_size+str._size>_capacity){
-        _capacity = std::nearest_power_2(_size+str._size+1);
-        char * tmp = new char[_capacity];
-        for(size_t i=0;i<_size;i++)
-            tmp[i]= _data[i];
-        delete[] _data;
-    }
-    for(size_t i=0;i<str._size;i++)
-        _data[_size+i]=str._data[i];
-    _size+=str._size;
-    _data[_size] = '\0';
-    return *this;
+string& string::operator+=(const string& str) {
+	return append(str._data, str._size);
 }
 
-std::string& std::string::operator+=(const char* str) {
-    size_t len = length(str);
+string& string::operator+=(const char* str) {
+	return append(str, length(str));
+}
+
+string& string::append (const char* str, size_t len) {
     if(_size+len>_capacity){
         _capacity = std::nearest_power_2(_size+len+1);
         char * tmp = new char[_capacity];
@@ -67,7 +62,9 @@ std::string& std::string::operator+=(const char* str) {
     return *this;
 }
 
-const char* std::string::c_str() const {
+const char* string::c_str() const {
     return _data;
+}
+
 }
 
