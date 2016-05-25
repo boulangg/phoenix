ad28aca5d96a0787372e40f3428f2f673438c272
boulangg <guillaume.boulanger68@sfr.fr>
2016-05-25 23:31:15 +0300

    Improve makefile and test2 sample

--- user/apps/test2/main.cpp
+++ user/apps/test2/main.cpp
@@ -1,8 +1,24 @@
 
+class A {
+public:
+	A(int i) : i(i) {}
+
+	static int a;
+	static A* b;
+
+	int i;
+};
+
+int A::a = 0;
+A* A::b = new A(3);
 
 int a = 1;
 
 int main() {
+	while(1) {
+
+	}
+	static int bss[8000];
 	a = 0;
 	return a;
 }
