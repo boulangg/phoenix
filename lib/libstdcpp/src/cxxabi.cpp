2ac3f93419b035dc50058c2c8c76b0e2b873d973
boulangg <guillaume.boulanger68@sfr.fr>
2016-05-29 23:31:42 +0300

    cxxabi for global objects

---
+++ lib/libstdcpp/src/cxxabi.cpp
@@ -0,0 +1,85 @@
+#include <cstdint>
+
+namespace __cxxabiv1
+{
+
+__extension__ typedef int __guard __attribute__((mode(__DI__)));
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ATEXIT_MAX_FUNCS	128
+
+struct atexit_func_entry_t {
+	void (*destructor_func)(void *);
+	void *obj_ptr;
+	void *dso_handle;
+};
+
+int __cxa_guard_acquire (__guard *);
+void __cxa_guard_release (__guard *);
+void __cxa_guard_abort (__guard *);
+int __cxa_atexit(void (*f)(void *), void *objptr, void *dso);
+void __cxa_finalize(void *f);
+int atexit(void (*f)(void));
+
+
+void *__dso_handle = 0;
+
+atexit_func_entry_t __atexit_funcs[ATEXIT_MAX_FUNCS];
+int64_t __atexit_func_count = 0;
+
+int __cxa_guard_acquire (__guard *g) {
+	return !*(char *)(g);
+}
+
+void __cxa_guard_release (__guard *g) {
+	*(char *)g = 1;
+}
+
+void __cxa_guard_abort (__guard *) {
+
+}
+
+int __cxa_atexit(void (*f)(void *), void *objptr, void *dso) {
+	if (__atexit_func_count >= ATEXIT_MAX_FUNCS) {
+		return -1;
+	}
+	__atexit_funcs[__atexit_func_count].destructor_func = f;
+	__atexit_funcs[__atexit_func_count].obj_ptr = objptr;
+	__atexit_funcs[__atexit_func_count].dso_handle = dso;
+	__atexit_func_count++;
+	return 0;
+}
+
+void __cxa_finalize(void *f) {
+	int64_t i = __atexit_func_count;
+	if (!f) {
+		while (i--) {
+			if (__atexit_funcs[i].destructor_func) {
+				(*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
+			}
+		}
+		return;
+	}
+
+	for ( ; i >= 0; --i) {
+		if (__atexit_funcs[i].destructor_func == f) {
+			(*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
+			__atexit_funcs[i].destructor_func = 0;
+		}
+	}
+}
+
+int atexit(void (*f)(void)) {
+	typedef void (*fn_ptr)(void*);
+	return __cxa_atexit((fn_ptr)f, nullptr, nullptr);
+}
+
+
+#ifdef __cplusplus
+};
+#endif
+
+}
