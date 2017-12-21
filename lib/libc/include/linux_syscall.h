49fdbdb15661d568a97721e6e570441e3672a0f6
boulangg <guillaume.boulanger68@sfr.fr>
2017-12-21 15:15:55 +0100

    linux_dirent64

--- lib/libc/include/linux_syscall.h
+++ lib/libc/include/linux_syscall.h
@@ -4,9 +4,9 @@
 struct linux_dirent64 {
 	unsigned long long d_ino;     /* Inode number */
 	unsigned long long d_off;     /* Offset to next linux_dirent */
-	unsigned short d_reclen;  /* Length of this linux_dirent */
-	unsigned char d_type;
-	char           d_name[0]; /* Filename (null-terminated) */
+	unsigned short     d_reclen;  /* Length of this linux_dirent */
+	unsigned char      d_type;
+	char               d_name[0]; /* Filename (null-terminated) */
 };
 
 #endif
