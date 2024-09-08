4db3ffd1377b13a5061a61d2000b57423e57e961
boulangg <cookiesgboulanger@gmail.com>
2024-09-08 22:05:33 +0200

    UnitTestFramework update

--- tools/UnitTestFramework/lib/src/TestFailureException.h
+++ tools/UnitTestFramework/lib/src/TestFailureException.h
@@ -7,40 +7,48 @@
 #pragma once
 
 #include <stdexcept>
-#include <string>
 #include <string.h>
+#include <string>
 
 #include "utils.h"
 
 class TestFailureException
 {
 public:
-	TestFailureException(std::string expected, std::string actual, std::string message)
-	{
-		std::string msg = message;
-		if (msg.empty()) {
-			msg += "Assert failed. Expected:<" + expected + "> Actual:<" + actual + ">";
-		}
+    TestFailureException(std::string expected, std::string actual, std::string message)
+    {
+        std::string msg = message;
+        if (msg.empty()) {
+            msg += "Assert failed. Expected:<" + expected + "> Actual:<" + actual + ">";
+        }
 
-		_message = new char[msg.size() + 1];
-		strncpy(_message, msg.c_str(), msg.size() + 1);
-	}
+        _message = new char[msg.size() + 1];
+        strncpy(_message, msg.c_str(), msg.size() + 1);
+    }
 
-	~TestFailureException()
-	{
-		if (_message != nullptr) {
-			delete _message;
-			_message = nullptr;
-		}
-	}
+    TestFailureException(std::string message)
+    {
+        std::string msg = message;
 
-	std::string getMessage() const
-	{
-		return std::string(_message);
-	}
+        _message = new char[msg.size() + 1];
+        strncpy(_message, msg.c_str(), msg.size() + 1);
+    }
+
+    ~TestFailureException()
+    {
+        if (_message != nullptr) {
+            delete _message;
+            _message = nullptr;
+        }
+    }
+
+    std::string getMessage() const
+    {
+        return std::string(_message);
+    }
 
 private:
-	// TestFailureException can't store std::string because the string are created in the testuite and then read by the test runner. 
-	// They could have different structure if they are not build with the same stdlib.
-	char* _message = nullptr;
+    // TestFailureException can't store std::string because the string are created in the testuite and then read by the
+    // test runner. They could have different structure if they are not build with the same stdlib.
+    char* _message = nullptr;
 };
--- tools/UnitTestFramework/lib/src/UnitTestFrameworkAssert.cpp
+++ tools/UnitTestFramework/lib/src/UnitTestFrameworkAssert.cpp
@@ -3,33 +3,46 @@
  * The file is distributed under the MIT license
  * The license is available in the LICENSE file or at https://github.com/boulangg/phoenix/blob/master/LICENSE
  */
- 
- #include "UnitTestFrameworkAssert.h"
+
+#include "UnitTestFrameworkAssert.h"
 
 namespace UnitTestFramework {
 
- void Assert::IsTrue(const bool& u, const std::string& message)
+void Assert::IsTrue(const bool& u, const std::string& message)
 {
-     if (!u) {
-         throw TestFailureException(std::to_string(true), std::to_string(u), message);
-     }
- }
+    if (!u) {
+        throw TestFailureException(std::to_string(true), std::to_string(u), message);
+    }
+}
 
- void Assert::IsTrue(const bool& u)
+void Assert::IsTrue(const bool& u)
 {
-     IsTrue(u, "");
- }
+    IsTrue(u, "");
+}
 
- void Assert::IsFalse(const bool& u, const std::string& message)
+void Assert::IsFalse(const bool& u, const std::string& message)
 {
-     if (u) {
-         throw TestFailureException(std::to_string(false), std::to_string(u), message);
-     }
- }
+    if (u) {
+        throw TestFailureException(std::to_string(false), std::to_string(u), message);
+    }
+}
 
- void Assert::IsFalse(const bool& u)
+void Assert::IsFalse(const bool& u)
 {
-     IsFalse(u, "");
- }
+    IsFalse(u, "");
+}
+
+void Assert::Fail(const std::string& message)
+{
+    throw TestFailureException(message);
+}
+
+template <>
+void Assert::AreEqual<std::string>(const std::string& u, const std::string& v, const std::string& message)
+{
+    if (u != v) {
+        throw TestFailureException(u, v, message);
+    }
+}
 
 }
